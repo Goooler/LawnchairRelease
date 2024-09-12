b1ffaa11982783e777f064d6cfb7f34684a0b34e
Goooler <wangzongler@gmail.com>
2024-09-12 17:16:09 +0800

    Fix style

--- lawnchair/src/app/lawnchair/ui/preferences/PreferenceActivity.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/PreferenceActivity.kt
@@ -20,7 +20,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
